c437f3f24b4b8bbad95f5405ff221f54e52c46ec
zepol3 <83396396+zepol3@users.noreply.github.com>
2022-11-15 12:55:15 +0100

    Add files via upload

---
+++ Practica 2-2/E5.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+
+int main(){
+
+    int file = open("prueba.txt", O_CREAT| O_CREAT | O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH|S_IXOTH);
+    
+    if(file == -1){
+        perror("Fallo");
+        return -1;
+    }
+    else{
+        close(file);
+        printf("el fichero se ha creado correctamente\n");
+    }
+
+    return 0;
+}
