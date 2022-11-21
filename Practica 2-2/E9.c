5978bff64ee21856b1eeb05872b63a6eb7f9b379
aalvaaro99 <alvlop14@ucm.es>
2022-11-21 10:21:43 +0100

    v3

---
+++ Practica 2-2/E9.c
@@ -0,0 +1,49 @@
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <time.h>
+#include <sys/sysmacros.h>
+
+int main(){
+
+    struct stat buff;
+
+    int file = open("prueba9.txt", O_CREAT| O_RDWR | O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH|S_IXOTH);
+ 
+
+    if(file == -1){
+        perror("No se ha conseguido abrir el archivo");
+        return -1;
+    }
+
+    if(fstat(file, &buff) == -1){
+        perror("No se han conseguido obtener los atributos")
+    }
+    else{
+        printf("Major: %d\n", major(buff.st_dev));
+        printf("Minor: %d\n", minor(buff.st_dev));
+        printf("Inode: %d\n", buff.st_ino);
+
+        char *tipo;
+
+        if(S_ISREG(buff.st_mode)){
+            tipo = "Fichero regular";
+        }
+
+        if(S_ISDIR(buff.st_mode)){
+            tipo = "Directorio";
+        }
+
+        if(S_ISLNK(buff.st_mode)){
+            tipo = "Enlace simbolico";
+        }
+        printf("Modo: %s\n", tipo);
+
+        printf("Last file modification:   %s", ctime(&sb.st_mtime));
+        printf("Last status change:       %s", ctime(&sb.st_ctime));
+    }
+
+}
