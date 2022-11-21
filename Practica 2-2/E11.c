c74cb824894e916d9498428b6cbbd03fc5dd61ef
aalvaaro99 <alvlop14@ucm.es>
2022-11-21 21:45:27 +0100

    V_correccion E11

--- Practica 2-2/E11.c
+++ Practica 2-2/E11.c
@@ -4,22 +4,42 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
-#include <time.h>
 #include <sys/sysmacros.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(int argc,char **argv){
 
     if(argc != 2){
-        perror("Tienes que pasar la ruta del fichero");
+        perror("Tienes que pasar la ruta del fichero\n");
         return -1;
     }
-
+    char* path = argv[1];
     struct stat _stat;
-    stat(argv[1], &_stat);
+    stat(path, &_stat);
 
     if(S_ISREG(_stat.st_mode)){     //si es un ficher regular modifico el tipo de enlace
-        string hardLink = string(argv[1])+ ".hard";
-        string symbolicLink = string(argv[1])+ ".sym";
+        char* hardLink = malloc(sizeof(char)*(strlen(path)+ strlen(".hard")));
+        char* symbolicLink = malloc(sizeof(char)*(strlen(path)+ strlen(".sym")));
         
+        hardLink = strcat(strcpy(hardLink, path), ".hard");
+        symbolicLink = strcat(strcpy(symbolicLink, path), ".sym");
 
+        if(link(path,hardLink) == 0){
+            printf("Exito al crear el enlace rigido\n");
+        }
+        else{
+            perror("No se ha conseguido crear el enlace rigido\n");
+        }
+
+        if(symlink(path, symbolicLink) == 0){
+            printf("Exito al crear el enlace simbolico\n");
+        }
+        else{
+            perror("No se ha conseguido crear el enlace simbolico\n");
+        }
     }
+    else{
+        perror("El path no es un fichero");
+    }
+}
