878c1626f5e6bcc693b42153ef1bf1d5f1691c4f
aalvaaro99 <alvlop14@ucm.es>
2022-11-27 19:44:10 +0100

    E17-v3

--- Practica 2-2/E17.c
+++ Practica 2-2/E17.c
@@ -23,30 +23,44 @@ int main(int argc,char **argv){
 
     struct stat informacion;
     struct dirent * actual;
-
+    off_t total = 0;
     while ((actual = readdir(directorio)) != NULL){
 
-        char *path = (char *) malloc(sizeof(char) * (strlen(argv[1]) + (actual -> d_name) + 3));
+        char *path = malloc(sizeof(char) *(strlen(argv[1]) + strlen(actual -> d_name) + 3));
 
         sprintf(path, "%s/%s", argv[1], actual -> d_name);
 
         if(stat(path, &informacion) == -1){
             perror("no se ha podido acceder a una de las entradas del directorio");
-            close(directorio);
+            closedir(directorio);
             return -1;
         }
 
         if(S_ISLNK(informacion.st_mode)){
+            char *linkname = (char * ) malloc( informacion.st_size + 1);
+            readlink(path, linkname, informacion.st_size + 1);
 
+            printf("%s -> %s\n", actual -> d_name, linkname);
         }
         else if(S_ISDIR(informacion.st_mode)){
+
+            printf("/%s\n", actual -> d_name);
 
         }
         else if (S_ISREG(informacion.st_mode))
         {
-            /* code */
+            printf("Fichero regular: %s", actual -> d_name);
+            if((informacion.st_mode & (S_IXUSR | S_IXGRP | S_IXOTH)) > 0){
+                    printf(" *");
+            }
+            printf("\n");
+            total += informacion.st_size;
         }
-        
+        free(path);
     }
-    
+
+    closedir(directorio);
+    float kb = total/ 1000;
+    printf("total kb: %f", kb);
+    return 0;
 }
